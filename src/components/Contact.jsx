8213044f34ff5d901f963b5d02a1a5bf1c6bd6f4
Matthew Haynes <matthew.haynes@joipolloi.com>
2019-06-08 17:16:24 +0100

    Change contact component to stateful

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/contact.scss';
 
-const Contact = () => (
-    <section className="contact">
-        <h3>Contact</h3>
-        <form method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-                <label>Your Name: <input type="text" name="name"/></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email"/></label>
-            </p>
-            <p>
-                <label>Subject: <input type="text" name="subject"/></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"> </textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-        </form>
-    </section>
-);
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+};
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+    }
+
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <section className="contact">
+                <h3>Get in touch</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <label>
+                            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+            </section>
+        );
+    }
+}
 
 export default Contact;
