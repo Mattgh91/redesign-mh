125f205f48587c721dbf8bae482a4443019f591a
Mattgh91 <Mattgh9152@gmail.com>
2019-06-28 17:16:35 +0100

    New slider quote; fix background-attachment on iPad/Phone

--- src/components/defaultSlides.js
+++ src/components/defaultSlides.js
@@ -7,12 +7,17 @@ const defaultSlides = [
     {
         title: "The problem with quotes on the Internet is that it is hard to verify their authenticity.",
         subtitle: "Abraham Lincoln",
-        image: "https://picsum.photos/400"
+        image: "https://picsum.photos/400",
     },
     {
         title: "The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past.",
         subtitle: "Tim Berners-Lee",
-        image: "https://picsum.photos/350"
+        image: "https://picsum.photos/350",
+    },
+    {
+        title: "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
+        subtitle: "Unknown",
+        image: "",
     },
 ];
 
