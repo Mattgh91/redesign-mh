659b8123b6dbd1570e8a87f47428c4b042a72070
Mattgh91 <Mattgh9152@gmail.com>
2019-08-08 19:42:59 +0100

    Add react-motion to project; convert Header.jsx from React Pose to React Motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,76 @@
 import React, { PureComponent } from 'react';
 import scrollToComponent from 'react-scroll-to-component';
-import { PoseGroup } from 'react-pose';
-import {
-    HeaderContainer,
-    HeaderItem,
-    HeaderUl,
-    HeaderLi,
-    HeaderLogoG,
-} from './Animations';
+import { motion } from 'framer-motion';
 import '../styles/header.scss';
 import '../styles/_BurgerMenu.scss';
+
+const headerContainer = {
+    notScrolled: {
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        boxShadow: '0 0 0px rgba(0, 0, 0, 0)',
+    },
+    scrolled: {
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26)',
+    },
+};
+const headerItem = {
+    notScrolled: {
+        color: '#fff',
+    },
+    scrolled: {
+        color: '#0980A5',
+    },
+};
+const headerLogoG = {
+    notScrolled: { fill: '#ffffff', },
+    scrolled: {
+        fill: '#111432',
+        transition: {
+            duration: 1,
+            delay: 1.25,
+        },
+    },
+};
+const mobileHeaderUl = {
+    show: {
+        backgroundColor: '#111432',
+        display: 'flex',
+        transition: {
+            staggerChildren: 0.15,
+            when: 'beforeChildren',
+            duration: 0.35,
+        },
+    },
+    hide: {
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        transition: {
+            staggerChildren: 0.15,
+            when: 'afterChildren',
+            duration: 0.35,
+            staggerDirection: -1,
+        },
+        transitionEnd: {
+            display: 'none',
+        },
+    },
+};
+const mobileHeaderLi = {
+    show: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: .25,
+        }
+    },
+    hide: {
+        y: 35,
+        opacity: 0,
+        transition: {
+            duration: .25,
+        }
+    },
+};
 
 class Header extends PureComponent {
     constructor(props){
@@ -70,12 +131,15 @@ class Header extends PureComponent {
         }
 
         return (
-            <HeaderContainer
-                pose={isScrolled ? 'scrolled' : 'notScrolled'}
+            <motion.header
+                animate={isScrolled ? 'scrolled' : 'notScrolled'}
+                variants={headerContainer}
                 className={isScrolled ? 'scrolled' : 'notScrolled'}
             >
-                <HeaderItem
+                <motion.a
                     href="#"
+                    variants={headerItem}
+                    animate={isScrolled ? 'scrolled' : 'notScrolled'}
                     onClick={
                         (e) => scrollToComponent(
                             landing,
@@ -86,11 +150,19 @@ class Header extends PureComponent {
                     className="logo"
                 >
                     <svg width="50px" height="50px" viewBox="0 0 192 192">
-                        <HeaderLogoG id="Page-1" stroke="#111432" strokeWidth="5" fill="#fff" fillRule="evenodd">
+                        <motion.g
+                            variants={headerLogoG}
+                            animate={isScrolled && !showNav ? 'scrolled' : 'notScrolled'}
+                            id="Page-1"
+                            stroke="#111432"
+                            strokeWidth="5"
+                            fill="#fff"
+                            fillRule="evenodd"
+                        >
                             <path d="M 0 1.13686838e-13 31.1503906 1.13686838e-13 62.4804688 151.501953 92.9492188 1.13686838e-13 123.033203 1.13686838e-13 123.033203 86.3964844 169.990234 86.3964844 169.990234 1.13686838e-13 192 1.13686838e-13 192 192 169.990234 192 169.990234 106.341797 123.033203 106.341797 123.033203 192 101.802734 192 101.802734 43.7714844 72.2304688 192 51.4726562 192 20.4707031 43.7714844 20.4707031 192 0 192z"/>
-                        </HeaderLogoG>
+                        </motion.g>
                     </svg>
-                </HeaderItem>
+                </motion.a>
 
                 <button
                     className={`mobile ${showNav ? 'open' : 'closed'}`}
@@ -109,10 +181,18 @@ class Header extends PureComponent {
                 </button>
 
                 <nav>
-                    <HeaderUl pose={showNav ? 'show' : 'hide'}>
-                        <PoseGroup>
-                            <HeaderLi key={0}>
-                                <HeaderItem
+                    <motion.ul
+                        animate={showNav ? 'show' : 'hide'}
+                        variants={mobileHeaderUl}
+                        initial={false}
+                    >
+                            <motion.li
+                                key={0}
+                                variants={mobileHeaderLi}
+                            >
+                                <motion.a
+                                    variants={headerItem}
+                                    animate={isScrolled ? 'scrolled' : 'notScrolled'}
                                     href="#aboutMe"
                                     onClick={
                                         (e) => {
@@ -126,10 +206,15 @@ class Header extends PureComponent {
                                     }
                                 >
                                     About Me
-                                </HeaderItem>
-                            </HeaderLi>
-                            <HeaderLi key={1}>
-                                <HeaderItem
+                                </motion.a>
+                            </motion.li>
+                            <motion.li
+                                key={1}
+                                variants={mobileHeaderLi}
+                            >
+                                <motion.a
+                                    variants={headerItem}
+                                    animate={isScrolled ? 'scrolled' : 'notScrolled'}
                                     href="#clients"
                                     onClick={
                                         (e) => {
@@ -144,10 +229,15 @@ class Header extends PureComponent {
 
                                 >
                                     My Work
-                                </HeaderItem>
-                            </HeaderLi>
-                            <HeaderLi key={2}>
-                                <HeaderItem
+                                </motion.a>
+                            </motion.li>
+                            <motion.li
+                                key={2}
+                                variants={mobileHeaderLi}
+                            >
+                                <motion.a
+                                    variants={headerItem}
+                                    animate={isScrolled ? 'scrolled' : 'notScrolled'}
                                     href="#contact"
                                     onClick={
                                         (e) => {
@@ -161,12 +251,11 @@ class Header extends PureComponent {
                                     }
                                 >
                                     Contact
-                                </HeaderItem>
-                            </HeaderLi>
-                        </PoseGroup>
-                    </HeaderUl>
+                                </motion.a>
+                            </motion.li>
+                </motion.ul>
                 </nav>
-            </HeaderContainer>
+            </motion.header>
         )
     }
 }
